0da96f61e4a2c265f376b569088bc8d805e039e1
RoseGabriely <rosegabriely14@gmail.com>
2021-10-21 21:57:39 -0400

    fixed schema for update acc info, connected to backend

--- plants/src/components/UpdateAccount.js
+++ plants/src/components/UpdateAccount.js
@@ -1,51 +1,59 @@
-// create a form to update passwords and phone numbers, save data to state
-// Dani
+import { useHistory } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import axiosWithAuth from "../util";
 import updateAccSchema from "../yup/updateAccSchema";
 
-const initialUpdateValues = {};
+const initialUpdateValues = {
+  password: "",
+  phone: "",
+};
 
 const initialUpdateErrors = {
   password: "",
-  phone: ""
+  phone: "",
 };
 
 const initialDisabled = true;
 
 const UpdateAccount = () => {
+  const { push } = useHistory();
+
   const [updateValues, setUpdateValues] = useState(initialUpdateValues);
   const [updateErrors, setUpdateErrors] = useState(initialUpdateErrors);
   const [disabled, setDisabled] = useState(initialDisabled);
 
   //*Validation
-  // const validate = (name, value) => {
-  //   yup
-  //     .reach(updateAccSchema, name)
-  //     .validate(value)
-  //     .then(() => setUpdateErrors({ ...updateErrors, [name]: "" }))
-  //     .catch((err) => setUpdateErrors({ ...updateErrors, [name]: err.errors[0] }));
-  // };
+  const validate = (name, value) => {
+    yup
+      .reach(updateAccSchema, name)
+      .validate(value)
+      .then(() => setUpdateErrors({ ...updateErrors, [name]: "" }))
+      .catch((err) =>
+        setUpdateErrors({ ...updateErrors, [name]: err.errors[0] })
+      );
+  };
 
   //*OnChange
   const onChange = (evt) => {
     const { name, value } = evt.target;
-    // validate(name, value);
-    console.log(name, value);
+    validate(name, value);
+    console.log(updateErrors);
     setUpdateValues({ ...updateValues, [name]: value });
   };
 
   //*Submit
   const onSubmit = (evt) => {
     evt.preventDefault();
-
     const username = localStorage.getItem("username");
-    // validate(updateValues);
     axiosWithAuth()
-      .put(`https://watergrows.herokuapp.com/api/users/update-account/${username}`, updateValues)
+      .put(
+        `https://watergrows.herokuapp.com/api/users/update-account/${username}`,
+        updateValues
+      )
       .then((res) => {
         console.log(res.data);
+        push("/plants");
       })
       .catch((err) => {
         console.log(err.response);
@@ -79,14 +87,13 @@ const UpdateAccount = () => {
             value={updateValues.phone}
             onChange={onChange}
             placeholder="Enter New Phone Number"
-            // pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
           />
         </label>
         <br />
-        <button>Save Changes</button>
+        <button disabled={disabled}>Save Changes</button>
       </form>
       <p style={{ color: "red" }}>{updateErrors.password}</p>
-      <p style={{ color: "red" }}>{updateErrors.phoneNumber}</p>
+      <p style={{ color: "red" }}>{updateErrors.phone}</p>
     </div>
   );
 };
--- plants/src/yup/updateAccSchema.js
+++ plants/src/yup/updateAccSchema.js
@@ -1,8 +1,26 @@
 import * as yup from "yup";
 
-const updateAccSchema = yup.object().shape({
-  password: yup.string().trim().min(6, "Password must be at least 6 characters").notRequired(),
-  phoneNumber: yup.string().min(10, "Phone number must have 10 digits").notRequired()
-});
+const updateAccSchema = yup.object().shape(
+  {
+    password: yup
+      .string()
+      .trim()
+      .when("phone", {
+        is: (phone) => !phone || phone.length === 0,
+        then: yup.string().min(5, "Password must be at least 5 characters"),
+        otherwise: yup.string(),
+      }),
+    phone: yup.string().when("password", {
+      is: (password) => !password || password.length === 0,
+      then: yup
+        .string()
+        .matches(/^[0-9]+$/, "Must be only digits")
+        .min(10, "Phone number must be 10 digits")
+        .max(10, "Phone number must be 10 digits"),
+      otherwise: yup.string(),
+    }),
+  },
+  [["password", "phone"]]
+);
 
 export default updateAccSchema;
